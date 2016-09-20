c26af693b61e503b300812225504adec2aaac599
dgp <dgp>
2016-09-20 19:40:51 +0000

    Some source code fixes contributed from FreeBSD ports.

--- generic/mpiface.c
+++ generic/mpiface.c
@@ -69,7 +69,7 @@ Mpexpr_Init (interp)
     Tcl_Interp *interp;
 {
     Mp_Data *mdPtr;
-    static initialized = 0;
+    static int initialized = 0;
     TCL_DECLARE_MUTEX(mpMutex)
 
     if (!initialized) {
